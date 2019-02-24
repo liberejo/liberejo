b894400dade6cc0529de896ab3463c0f1d1f0acd
Cassaundra <cass@cassaundra.io>
2019-02-23 17:21:51 -0800

    Implement render, resize, and dispose

--- core/src/main/kotlin/org/liberejo/game/GameScreen.kt
+++ core/src/main/kotlin/org/liberejo/game/GameScreen.kt
@@ -3,6 +3,7 @@ package org.liberejo.game
 import com.badlogic.ashley.core.PooledEngine
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputMultiplexer
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Stage
@@ -109,7 +110,27 @@ class GameScreen(isClient: Boolean, isServer: Boolean, address: String = "localh
 
 	}
 
+	override fun render(delta: Float) {
+		// clear
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+		// update engine
+		engine.update(delta)
+
+		// render ui
+		uiStage.act(delta)
+		uiStage.draw()
+	}
+
+	override fun resize(width: Int, height: Int) {
+		uiStage.viewport.update(width, height, true)
+		renderingSystem.resize(width, height)
+	}
+
 	override fun dispose() {
+		uiStage.dispose()
 		networkManager.stop()
+		engine.clearPools()
 	}
 }
