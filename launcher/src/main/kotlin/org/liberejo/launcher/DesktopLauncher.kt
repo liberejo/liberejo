44ab74997f3867cf62bca05f7a5ecc7c4efc4912
Cassaundra <cass@cassaundra.io>
2019-03-02 16:41:07 -0800

    Modify DesktopLauncher to prepare for package testing

--- launcher/src/main/kotlin/org/liberejo/launcher/DesktopLauncher.kt
+++ launcher/src/main/kotlin/org/liberejo/launcher/DesktopLauncher.kt
@@ -5,13 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import org.liberejo.game.Liberejo
 
 object DesktopLauncher {
-	@JvmStatic
-	fun main(arg: Array<String>) {
+	@JvmStatic fun main(args: Array<String>) {
+		startDesktopApp()
+	}
+
+	fun startDesktopApp() {
 		val config = LwjglApplicationConfiguration()
 		config.apply {
 			width = 1280
 			height = 720
-			title = "liberejo"
 		}
 		LwjglApplication(Liberejo, config)
 	}
